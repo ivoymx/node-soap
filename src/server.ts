2dc80c6cbca90dd6e85bcae962378446d3534996
Christian España <espanasilva33@gmail.com>
2019-11-07 16:32:23 -0600

    feat: add content type header

--- src/server.ts
+++ src/server.ts
@@ -641,7 +641,7 @@ export class Server extends EventEmitter {
     * Node.js to calculate and send Content-Length header. See
     * nodejs/node#26005.
     */
-    res.writeHead(200, {'Content-Type': 'application/soap+xml, application/dime, multipart/related, text/*','Content-Length':result.toString().length+''});
+    res.writeHead(200, {'Content-Type': 'text/xml;charset=utf-8','Content-Length':result.toString().length+''});
         
     if (this.enableChunkedEncoding) {
       res.write(result);
